50d2a68e89f0e2e07c3be8ed972c62510d7eef8c
dotitsolution-ab <dotitsolution.com@gmail.com>
2026-05-21 00:55:53 +0600

    Configure Next routes for Cloudflare Edge runtime

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -19,6 +19,8 @@ type PageProps = {
   }>;
 };
 
+export const dynamicParams = false;
+
 export function generateStaticParams() {
   return allRoutes.map((route) => ({
     slug: route === "/" ? [] : route.split("/").filter(Boolean),
